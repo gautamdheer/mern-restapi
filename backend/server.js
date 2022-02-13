57242231c41e3483849bd5deaa2705793e33e0da
Gautam Dheer <gautam.litesh@gmail.com>
2022-02-13 22:58:34 +0530

    intial commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,12 @@
-console.log('====================================');
-console.log('hello world');
-console.log('====================================');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/goals/', require('./routes/goalsRoutes') )
+
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
