5babb7efd3a0e358bbf3d61856dd08194fc16e9b
Gautam Dheer <gautam.litesh@gmail.com>
2022-02-14 17:27:55 +0530

    add seperate goals router file

--- backend/routes/goalsRoutes.js
+++ backend/routes/goalsRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.status(200).json({message : "Get Goals"})
 })
-router.post('/', (req, res)=>{
+router.post('/', (req, res) => {
     res.status(200).json({message : "Set goal"})
 })
-router.get('/', (req, res)=>{
-    res.status(200).json({message : "Get Goals"})
-})
-router.get('/', (req, res)=>{
-    res.status(200).json({message : "Get Goals"})
-})
 
+router.get('/', (req, res) => {
+    res.status(200).json({message : "Get Goals"})
+})
+router.get('/', (req, res) => {
+    res.status(200).json({message : "Get Goals"})
+})
 
 module.exports = router;
