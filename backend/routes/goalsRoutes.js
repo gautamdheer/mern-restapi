dc635aba470acd323b0f21ac3002f379059f3b5f
Gautam Dheer <gautam.litesh@gmail.com>
2022-04-06 19:17:52 +0530

    after updating routers file

--- backend/routes/goalsRoutes.js
+++ backend/routes/goalsRoutes.js
@@ -3,13 +3,9 @@ const router = express.Router()
 
 const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
 
-// router.route()
 
-router.get('/', getGoals)
-router.post('/', setGoal)
-
-router.put('/:id',updateGoal)
-router.delete('/:id',deleteGoal)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
  
 
 module.exports = router;
